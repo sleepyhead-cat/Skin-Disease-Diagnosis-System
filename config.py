fa79853b58912e8707463e9f67b6c04cad4373cd
KoySokun <koy.sokun27@gmail.com>
2026-09-07 14:48:11 +0700

    Update Config

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+
+os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
+
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL")
-        or "sqlite:///" + os.path.join(BASE_DIR, "instance", "skindisease.db")
+        or "sqlite:///" + os.path.join(INSTANCE_DIR, "skindisease.db")
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
